63d12cffda21a031c004405597aed9dc7dbb4735
KhaosBlaze <khaosblaze@gmail.com>
2019-08-03 11:49:46 -0400

    Update Stan_the_man.py

--- Stan_the_man.py
+++ Stan_the_man.py
@@ -14,19 +14,28 @@ async def on_ready():
 	print("Ready for insults!")
 
 @bot.command(pass_context=True)
-async def ping(ctx):
-	await bot.say(":ping_pong: ping!!")
-	print("user has pinged")
-
-@bot.command(pass_context=True)
 async def user(ctx):
 	await bot.say(ctx.message.author)
 
 @bot.command(pass_context=True)
-async def is_stanley_awake():
+async def done_yet():
 	for proc in psutil.process_iter():
 		if proc.name().lower() == 'python' and 'stanley.py' in proc.cmdline():
-			await bot.say("Ya boi still chuggin")
+			await bot.say("https://media1.tenor.com/images/a4f8ddfe01f388d1abc7022150398bc8/tenor.gif?itemid=4535581")
+
+
+@bot.command(pass_context=True)
+async def results():
+	checker = []
+	while open("test.out","r") as reults:
+		for line in results:
+			checker.append()
+
+	if all(x == checker[0] for x in chcker):
+		await bot.say("https://media1.tenor.com/images/f89c189082d675ca5d27eb5028969beb/tenor.gif?itemid=10555880")
+	else
+		await bot.say("https://i.redd.it/usiqjl0cbyr11.jpg")
+
 
 
 bot.run("NDY2MTE4MDU3ODQwODY5Mzc2.DiXk5g.gOzcGyDg8W__fizmj1pBmrXQzVA")
