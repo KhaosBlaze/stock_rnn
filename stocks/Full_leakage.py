bb3b3a6ebebd661cc524db5d25b3a5e1bdcc3beb
KhaosBlaze <khaosblaze@gmail.com>
2019-08-13 23:07:10 -0400

    Update Full_leakage.py

--- stocks/Full_leakage.py
+++ stocks/Full_leakage.py
@@ -1,4 +1,10 @@
 import numpy as np
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import LeakyReLU
+from keras import optimizers
+from keras.models import Sequential
 from subconscious import get_X_Y, survey_says, get_a_symbol, save_it
 
 days_to_train_on = 45
